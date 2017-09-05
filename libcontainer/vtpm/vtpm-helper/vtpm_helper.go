f915b4dbf312908e57b358638f2fc6d942f002f9
Stefan Berger <stefanb@linux.vnet.ibm.com>
2017-09-06 00:00:08 +0200

    tpm: Use the host device name inside the container
    The host device name has to be used inside the container for the
    major and minor numbers to appear correctly.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- libcontainer/vtpm/vtpm-helper/vtpm_helper.go
+++ libcontainer/vtpm/vtpm-helper/vtpm_helper.go
@@ -15,10 +15,10 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func addVTPMDevice(spec *specs.Spec, config *configs.Config, devpath string, major, minor uint32) {
+func addVTPMDevice(spec *specs.Spec, config *configs.Config, hostpath string, major, minor uint32) {
 	device := &configs.Device{
 		Type:        'c',
-		Path:        devpath,
+		Path:        hostpath,
 		Major:       int64(major),
 		Minor:       int64(minor),
 		Permissions: "rwm",
@@ -63,8 +63,7 @@ func CreateVTPM(spec *specs.Spec, config *configs.Config, vtpmdev *specs.VTPM, d
 	hostdev := vtpm.GetTPMDevname()
 	major, minor := vtpm.GetMajorMinor()
 
-	devpath := fmt.Sprintf("/dev/tpm%d", devnum)
-	addVTPMDevice(spec, config, devpath, major, minor)
+	addVTPMDevice(spec, config, hostdev, major, minor)
 
 	config.VTPMs = append(config.VTPMs, vtpm)
 
@@ -81,8 +80,7 @@ func CreateVTPM(spec *specs.Spec, config *configs.Config, vtpmdev *specs.VTPM, d
 	if fileInfo, err := os.Lstat(host_tpmrm); err == nil {
 		if stat_t, ok := fileInfo.Sys().(*syscall.Stat_t); ok {
 			devNumber := stat_t.Rdev
-			devpath = fmt.Sprintf("/dev/tpmrm%d", devnum)
-			addVTPMDevice(spec, config, devpath, uint32(unix.Major(devNumber)), uint32(unix.Minor(devNumber)))
+			addVTPMDevice(spec, config, host_tpmrm, uint32(unix.Major(devNumber)), uint32(unix.Minor(devNumber)))
 		}
 		if uid != 0 {
 			// adapt ownership of the device since only root can access it
